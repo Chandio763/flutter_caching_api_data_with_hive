4b384604c4b30ae536551623c9eba7cafc28f2c7
Kashif Chandio <kashikha763@gmail.com>
2022-01-24 17:34:02 +0000

    ...

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -6,36 +6,36 @@ import 'package:flutter_caching_api_data_with_hive/Constants.dart';
 import 'package:flutter_caching_api_data_with_hive/model/post.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:http/http.dart' as http;
-class ApiServices{
 
+class ApiServices {
   static Future<List<UserPost>> getPosts() async {
     //print('Method Called');
     List<UserPost> posts = [];
-    Hive.openBox(Constants.APIDATA_BOX);
+    var box =await Hive.openBox(Constants.APIDATA_BOX);
     try {
-      var response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'),);
-      //print(response.body);
-    if (response.statusCode == 200) {
-      print(response.statusCode);
-      Hive.box(Constants.APIDATA_BOX).clear();
-      Hive.box(Constants.APIDATA_BOX).put(Constants.API_DATA,jsonDecode(response.body));
-      
-    }
+      var response = await http.get(
+        Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+      );
+      if (response.statusCode == 200) {
+        print(response.statusCode);
+        await box.clear();
+        await box.put(Constants.API_DATA, jsonDecode(response.body));
+      }
     } catch (e) {
-      print('object');
+      print('You are not connected to internet');
     }
-    var listMaps =await Hive.box(Constants.APIDATA_BOX).get(Constants.API_DATA, defaultValue: []);
-    listMaps.map<UserPost>((map) {
-          print(map);
-          posts.add(UserPost.fromMap(map));
-          return UserPost.fromMap(map);
-      }).toList();
-    //print('data from hive $listMaps');
-    // for (var item in listMaps) {
-    //     posts.add(UserPost.fromMap(item));
-    //     print('item Added to Post $item');
-    // }
-  print(posts.length);
-  return posts;
+    var listMaps = await box.get(Constants.API_DATA, defaultValue: []);
+    // listMaps.map<UserPost>((map) {
+    //   print(map);
+    //   posts.add(UserPost.fromMap(map));
+    //   return UserPost.fromMap(map);
+    // }).toList();
+    print('data from hive $listMaps');
+    for (var item in listMaps) {
+        posts.add(UserPost.fromMap(item));
+        print('item Added to Post $item');
+    }
+    print(posts.length);
+    return posts;
   }
 }
